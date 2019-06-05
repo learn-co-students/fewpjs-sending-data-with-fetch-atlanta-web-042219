16ecb86eea901ac16d04a7be3743c3abc2e28274
Han Xu <hanxu27@gmail.com>
2019-06-04 20:44:12 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your code here
+
+function submitData(name, email) {
+  let formData = {
+    name,
+    email
+  }
+  
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+  }
+  
+  return fetch("http://localhost:3000/users", configObj)
+    .then(resp => resp.json())
+    .then(obj => document.body.innerHTML = obj.id)
+    .catch(function(error) { 
+      alert("bad things!")
+      document.body.innerHTML = error.message
+    })
+}
